7871827f568801e8847f8fb80cc81fbe4f879157
sonnv10121997 <sonnv10121997@gmail.com>
2023-08-07 16:37:40 +0700

    Split variables

--- main.tf
+++ main.tf
@@ -10,232 +10,229 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region = var.region
 }
 
-resource "aws_vpc" "lab-1-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags       = { Name = "lab-1-vpc" }
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc.cidr_block
+  tags       = { Name = "vpc" }
 }
 
 ### PUBLIC SUBNETS
-resource "aws_subnet" "lab-1-subnet-public-1a" {
-  vpc_id            = aws_vpc.lab-1-vpc.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "ap-southeast-1a"
-  tags              = { Name = "lab-1-subnet-public-1a" }
+resource "aws_subnet" "subnet_public_1a" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnets["public_1a"].cidr_block
+  availability_zone = var.subnets["public_1a"].availability_zone
+  tags              = { Name = "subnet-public-1a" }
 }
 
-resource "aws_subnet" "lab-1-subnet-public-1b" {
-  vpc_id            = aws_vpc.lab-1-vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-southeast-1b"
-  tags              = { Name = "lab-1-subnet-public-1b" }
+resource "aws_subnet" "subnet_public_1b" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnets["public_1b"].cidr_block
+  availability_zone = var.subnets["public_1b"].availability_zone
+  tags              = { Name = "subnet-public-1b" }
 }
 
-resource "aws_subnet" "lab-1-subnet-public-1c" {
-  vpc_id            = aws_vpc.lab-1-vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-southeast-1c"
-  tags              = { Name = "lab-1-subnet-public-1c" }
+resource "aws_subnet" "subnet_public_1c" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnets["public_1c"].cidr_block
+  availability_zone = var.subnets["public_1c"].availability_zone
+  tags              = { Name = "subnet-public-1c" }
 }
 
-resource "aws_internet_gateway" "lab-1-igw" {
-  vpc_id = aws_vpc.lab-1-vpc.id
-  tags   = { Name = "lab-1-igw" }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags   = { Name = "igw" }
 }
 
-resource "aws_route_table" "lab-1-rtb-public" {
-  vpc_id = aws_vpc.lab-1-vpc.id
-  tags   = { Name = "lab-1-rtb-public" }
+resource "aws_default_route_table" "rtb_public" {
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
+  tags                   = { Name = "rtb-public" }
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.lab-1-igw.id
+    cidr_block = var.rtbs["public"].cidr_block
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
 
-resource "aws_route_table_association" "lab-1-rtb-public-association-1a" {
-  subnet_id      = aws_subnet.lab-1-subnet-public-1a.id
-  route_table_id = aws_route_table.lab-1-rtb-public.id
+resource "aws_route_table_association" "rtb_public_association_1a" {
+  subnet_id      = aws_subnet.subnet_public_1a.id
+  route_table_id = aws_default_route_table.rtb_public.id
 }
 
-resource "aws_route_table_association" "lab-1-rtb-public-association-1b" {
-  subnet_id      = aws_subnet.lab-1-subnet-public-1b.id
-  route_table_id = aws_route_table.lab-1-rtb-public.id
+resource "aws_route_table_association" "rtb_public_association_1b" {
+  subnet_id      = aws_subnet.subnet_public_1b.id
+  route_table_id = aws_default_route_table.rtb_public.id
 }
 
-resource "aws_route_table_association" "lab-1-rtb-public-association-1c" {
-  subnet_id      = aws_subnet.lab-1-subnet-public-1c.id
-  route_table_id = aws_route_table.lab-1-rtb-public.id
+resource "aws_route_table_association" "rtb_public_association_1c" {
+  subnet_id      = aws_subnet.subnet_public_1c.id
+  route_table_id = aws_default_route_table.rtb_public.id
 }
 
 ### PRIVATE SUBNETS
-resource "aws_subnet" "lab-1-subnet-private-1a" {
-  vpc_id            = aws_vpc.lab-1-vpc.id
-  cidr_block        = "10.0.16.0/20"
-  availability_zone = "ap-southeast-1a"
-  tags              = { Name = "lab-1-subnet-private-1a" }
+resource "aws_subnet" "subnet_private_1a" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnets["private_1a"].cidr_block
+  availability_zone = var.subnets["private_1a"].availability_zone
+  tags              = { Name = "subnet-private-1a" }
 }
 
-resource "aws_subnet" "lab-1-subnet-private-1b" {
-  vpc_id            = aws_vpc.lab-1-vpc.id
-  cidr_block        = "10.0.32.0/20"
-  availability_zone = "ap-southeast-1b"
-  tags              = { Name = "lab-1-subnet-private-1b" }
+resource "aws_subnet" "subnet_private_1b" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnets["private_1b"].cidr_block
+  availability_zone = var.subnets["private_1b"].availability_zone
+  tags              = { Name = "subnet-private-1b" }
 }
 
-resource "aws_subnet" "lab-1-subnet-private-1c" {
-  vpc_id            = aws_vpc.lab-1-vpc.id
-  cidr_block        = "10.0.48.0/20"
-  availability_zone = "ap-southeast-1c"
-  tags              = { Name = "lab-1-subnet-private-1c" }
+resource "aws_subnet" "subnet_private_1c" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnets["private_1c"].cidr_block
+  availability_zone = var.subnets["private_1c"].availability_zone
+  tags              = { Name = "subnet-private-1c" }
 }
 
-resource "aws_eip" "lab-1-eip" {
+resource "aws_eip" "eip" {
 
 }
 
-resource "aws_nat_gateway" "lab-1-ngw" {
-  subnet_id     = aws_subnet.lab-1-subnet-public-1a.id
-  allocation_id = aws_eip.lab-1-eip.id
-  tags          = { Name = "lab-1-ngw" }
-  depends_on    = [aws_internet_gateway.lab-1-igw]
+resource "aws_nat_gateway" "nat" {
+  subnet_id     = aws_subnet.subnet_public_1a.id
+  allocation_id = aws_eip.eip.id
+  depends_on    = [aws_internet_gateway.igw]
+  tags          = { Name = "nat" }
 }
 
-resource "aws_route_table" "lab-1-rtb-private" {
-  vpc_id = aws_vpc.lab-1-vpc.id
-  tags   = { Name = "lab-1-rtb-private" }
+resource "aws_route_table" "rtb_private" {
+  vpc_id = aws_vpc.vpc.id
+  tags   = { Name = "rtb-private" }
 
   route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.lab-1-ngw.id
+    cidr_block     = var.rtbs["private"].cidr_block
+    nat_gateway_id = aws_nat_gateway.nat.id
   }
 }
 
-resource "aws_route_table_association" "lab-1-rtb-private-association-1a" {
-  subnet_id      = aws_subnet.lab-1-subnet-private-1a.id
-  route_table_id = aws_route_table.lab-1-rtb-private.id
+resource "aws_route_table_association" "rtb_private_association_1a" {
+  subnet_id      = aws_subnet.subnet_private_1a.id
+  route_table_id = aws_route_table.rtb_private.id
 }
 
-resource "aws_route_table_association" "lab-1-rtb-private-association-1b" {
-  subnet_id      = aws_subnet.lab-1-subnet-private-1b.id
-  route_table_id = aws_route_table.lab-1-rtb-private.id
+resource "aws_route_table_association" "rtb_private_association_1b" {
+  subnet_id      = aws_subnet.subnet_private_1b.id
+  route_table_id = aws_route_table.rtb_private.id
 }
 
-resource "aws_route_table_association" "lab-1-rtb-private-association-1c" {
-  subnet_id      = aws_subnet.lab-1-subnet-private-1c.id
-  route_table_id = aws_route_table.lab-1-rtb-private.id
+resource "aws_route_table_association" "rtb_private_association_1c" {
+  subnet_id      = aws_subnet.subnet_private_1c.id
+  route_table_id = aws_route_table.rtb_private.id
 }
 
 ### BASTION HOST
-resource "aws_security_group" "lab-1-sg-bastion-host" {
-  vpc_id = aws_vpc.lab-1-vpc.id
-  name   = "lab-1-sg-bastion-host"
-  tags   = { Name = "lab-1-sg-bastion-host" }
+resource "aws_security_group" "sg_bastion_host" {
+  vpc_id = aws_vpc.vpc.id
+  tags   = { Name = "sg-bastion-host" }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = var.sgrs["bastion_host_ingress"].from_port
+    to_port     = var.sgrs["bastion_host_ingress"].to_port
+    protocol    = var.sgrs["bastion_host_ingress"].protocol
+    cidr_blocks = var.sgrs["bastion_host_ingress"].cidr_blocks
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = var.sgrs["bastion_host_egress"].from_port
+    to_port     = var.sgrs["bastion_host_egress"].to_port
+    protocol    = var.sgrs["bastion_host_egress"].protocol
+    cidr_blocks = var.sgrs["bastion_host_egress"].cidr_blocks
   }
 }
 
-resource "aws_key_pair" "lab-1-key-bastion-host" {
-  key_name   = "lab-1-key-bastion-host"
-  public_key = var.lab-1-public-key-bastion-host
+resource "aws_key_pair" "key_bastion_host" {
+  key_name   = "key-bastion-host"
+  public_key = var.keys["bastion_host"]
 }
 
-resource "aws_instance" "lab-1-bastion-host" {
-  ami                         = "ami-0df7a207adb9748c7"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  subnet_id                   = aws_subnet.lab-1-subnet-public-1a.id
-  vpc_security_group_ids      = [aws_security_group.lab-1-sg-bastion-host.id]
-  key_name                    = aws_key_pair.lab-1-key-bastion-host.key_name
-  tags                        = { Name = "lab-1-bastion-host" }
+resource "aws_instance" "bastion_host" {
+  ami                         = var.instances["bastion_host"].ami
+  instance_type               = var.instances["bastion_host"].instance_type
+  associate_public_ip_address = var.instances["bastion_host"].associate_public_ip_address
+  subnet_id                   = aws_subnet.subnet_public_1a.id
+  vpc_security_group_ids      = [aws_security_group.sg_bastion_host.id]
+  key_name                    = aws_key_pair.key_bastion_host.key_name
+  tags                        = { Name = "bastion-host" }
 }
 
 ### PRIVATE APP
-resource "aws_security_group" "lab-1-sg-private-app" {
-  vpc_id = aws_vpc.lab-1-vpc.id
-  name   = "lab-1-sg-private-app"
-  tags   = { Name = "lab-1-sg-private-app" }
+resource "aws_security_group" "sg_private_app" {
+  vpc_id = aws_vpc.vpc.id
+  tags   = { Name = "sg-private-app" }
 
   ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lab-1-sg-bastion-host.id]
+    from_port       = var.sgrs["private_app_ingress"].from_port
+    to_port         = var.sgrs["private_app_ingress"].to_port
+    protocol        = var.sgrs["private_app_ingress"].protocol
+    security_groups = [aws_security_group.sg_bastion_host.id]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = var.sgrs["private_app_egress"].from_port
+    to_port     = var.sgrs["private_app_egress"].to_port
+    protocol    = var.sgrs["private_app_egress"].protocol
+    cidr_blocks = var.sgrs["private_app_egress"].cidr_blocks
   }
 }
 
-resource "aws_key_pair" "lab-1-key-private-app" {
-  key_name   = "lab-1-key-private-app"
-  public_key = var.lab-1-public-key-private-app
+resource "aws_key_pair" "key_private_app" {
+  key_name   = "key-private-app"
+  public_key = var.keys["private_app"]
 }
 
-resource "aws_instance" "lab-1-private-app" {
-  ami                    = "ami-0df7a207adb9748c7"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.lab-1-subnet-private-1a.id
-  vpc_security_group_ids = [aws_security_group.lab-1-sg-private-app.id]
-  key_name               = aws_key_pair.lab-1-key-private-app.key_name
-  tags                   = { Name = "lab-1-private-app" }
+resource "aws_instance" "private_app" {
+  ami                    = var.instances["bastion_host"].ami
+  instance_type          = var.instances["bastion_host"].instance_type
+  subnet_id              = aws_subnet.subnet_private_1a.id
+  vpc_security_group_ids = [aws_security_group.sg_private_app.id]
+  key_name               = aws_key_pair.key_private_app.key_name
+  tags                   = { Name = "private-app" }
 }
 
 ### PRIVATE RDS
-resource "aws_db_subnet_group" "lab-1-subnet-group-private-rds" {
-  name       = "lab-1-subnet-group-private-rds"
-  subnet_ids = [aws_subnet.lab-1-subnet-private-1b.id, aws_subnet.lab-1-subnet-private-1c.id]
-  tags       = { Name = "lab-1-subnet-group-private-rds" }
+resource "aws_db_subnet_group" "subnet_group_private_rds" {
+  name       = "subnet-group-private-rds"
+  subnet_ids = [aws_subnet.subnet_private_1b.id, aws_subnet.subnet_private_1c.id]
+  tags       = { Name = "subnet-group-private-rds" }
 }
 
-resource "aws_security_group" "lab-1-sg-private-rds" {
-  vpc_id = aws_vpc.lab-1-vpc.id
-  name   = "lab-1-sg-private-rds"
-  tags   = { Name = "lab-1-sg-private-rds" }
+resource "aws_security_group" "sg_private_rds" {
+  vpc_id = aws_vpc.vpc.id
+  tags   = { Name = "sg-private-rds" }
 
   ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lab-1-sg-private-app.id]
+    from_port       = var.sgrs["private_rds_ingress"].from_port
+    to_port         = var.sgrs["private_rds_ingress"].to_port
+    protocol        = var.sgrs["private_rds_ingress"].protocol
+    security_groups = [aws_security_group.sg_private_app.id]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = var.sgrs["private_rds_egress"].from_port
+    to_port     = var.sgrs["private_rds_egress"].to_port
+    protocol    = var.sgrs["private_rds_egress"].protocol
+    cidr_blocks = var.sgrs["private_rds_egress"].cidr_blocks
   }
 }
 
-resource "aws_db_instance" "lab-1-private-rds" {
-  allocated_storage      = 10
-  engine                 = "postgres"
-  engine_version         = "15.3"
-  identifier             = "lab-1-private-rds"
-  username               = var.lab-1-private-rds-username
-  password               = var.lab-1-private-rds-password
-  instance_class         = "db.t3.micro"
-  db_subnet_group_name   = aws_db_subnet_group.lab-1-subnet-group-private-rds.name
-  vpc_security_group_ids = [aws_security_group.lab-1-sg-private-rds.id]
-  availability_zone      = "ap-southeast-1b"
-  skip_final_snapshot    = true
+resource "aws_db_instance" "private_rds" {
+  allocated_storage      = var.db_instances["private_rds"].allocated_storage
+  engine                 = var.db_instances["private_rds"].engine
+  engine_version         = var.db_instances["private_rds"].engine_version
+  identifier             = var.db_instances["private_rds"].identifier
+  username               = var.db_instances["private_rds"].username
+  password               = var.db_instances["private_rds"].password
+  instance_class         = var.db_instances["private_rds"].instance_class
+  db_subnet_group_name   = aws_db_subnet_group.subnet_group_private_rds.name
+  vpc_security_group_ids = [aws_security_group.sg_private_rds.id]
+  availability_zone      = var.db_instances["private_rds"].availability_zone
+  skip_final_snapshot    = var.db_instances["private_rds"].skip_final_snapshot
 }
--- variables.tf
+++ variables.tf
@@ -1,21 +1,65 @@
-variable "lab-1-private-rds-username" {
-  description = "Database administrator username"
-  type        = string
-  sensitive   = true
-}
-
-variable "lab-1-private-rds-password" {
-  description = "Database administrator password"
-  type        = string
-  sensitive   = true
-}
-
-variable "lab-1-public-key-bastion-host" {
-  description = "Public key for bastion host"
+variable "region" {
+  description = "AWS region"
   type        = string
 }
 
-variable "lab-1-public-key-private-app" {
-  description = "Public key for private app"
-  type        = string
+variable "vpc" {
+  description = "VPC configuration"
+  type = object({
+    cidr_block = string
+  })
+}
+
+variable "subnets" {
+  description = "Subnets map configuration"
+  type = map(object({
+    cidr_block        = string
+    availability_zone = string
+  }))
+}
+
+variable "rtbs" {
+  description = "Route tables map configuration"
+  type = map(object({
+    cidr_block = string
+  }))
+}
+
+variable "sgrs" {
+  description = "Security group rules map configuration"
+  type = map(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = optional(list(string))
+  }))
+}
+
+variable "keys" {
+  description = "Public keys map"
+  type        = map(string)
+}
+
+variable "instances" {
+  description = "EC2 instances configuration"
+  type = map(object({
+    ami                         = string
+    instance_type               = string
+    associate_public_ip_address = optional(bool)
+  }))
+}
+
+variable "db_instances" {
+  description = "DB instances configuration"
+  type = map(object({
+    allocated_storage   = number
+    engine              = string
+    engine_version      = string
+    identifier          = string
+    username            = string
+    password            = string
+    instance_class      = string
+    availability_zone   = string
+    skip_final_snapshot = bool
+  }))
 }
